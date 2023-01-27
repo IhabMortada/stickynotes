18965efab41e0c95f44fe7b3d4c665acf1a588e0
Ihab <ihab.mortada1@gmail.com>
2023-01-27 13:16:14 +0100

    users-notes-controllers-apis

--- backend/server.js
+++ backend/server.js
@@ -2,27 +2,32 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
-const PORT = process.env.PORT || 3500
-const {logger} = require('./middleware/logger')
+const { logger, logEvents } = require('./middleware/logger')
 const errorHandler = require('./middleware/errorHandler')
 const cookieParser = require('cookie-parser')
-const cors =require('cors')
-const corsOptions=require('./config/corsOptions')
-const connectDb = require('./config/dbConn')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
+const connectDB = require('./config/dbConn')
 const mongoose = require('mongoose')
-const { logEvents} = require('./middleware/logger')
- 
+const PORT = process.env.PORT || 3500
 
-connectDb()
-//built in middlewares
+console.log(process.env.NODE_ENV)
+
+connectDB()
 
 app.use(logger)
+
 app.use(cors(corsOptions))
+
 app.use(express.json())
-app.use(cookieParser)
-app.use('/', express.static(path.join(__dirname, 'public')))  // telling express where finding static files are using on the server
+
+app.use(cookieParser())
+
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/root'))
+app.use('/users', require('./routes/userRoutes'))
+app.use('/notes', require('./routes/noteRoutes'))
 
 app.all('*', (req, res) => {
     res.status(404)
@@ -30,21 +35,19 @@ app.all('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'views', '404.html'))
     } else if (req.accepts('json')) {
         res.json({ message: '404 Not Found' })
-    }
-    else {
-        res.type('txt'.send('404 Not Found'))
+    } else {
+        res.type('txt').send('404 Not Found')
     }
 })
-app.use (errorHandler)
 
-mongoose.connection.once('open',()=>{
-console.log( 'Connected to MongoDB')
+app.use(errorHandler)
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
-
-
 })
-mongoose.connection.on('error',err=>{
+
+mongoose.connection.on('error', err => {
     console.log(err)
     logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
-
 })
